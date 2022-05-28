c3f9e335b42de2823ec76c4a2f3e40036bc5e2d1
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-05-28 13:14:45 +0800

    Adjust spacing of elements

--- assets/js/timeClock.js
+++ assets/js/timeClock.js
@@ -2,11 +2,12 @@ const clockElement = document.querySelector('[data-clock]')
 
 function showTime() {
   let currentDate = new Date()
-  // let currentTime = currentDate.toLocaleTimeString([], {
-  //   hour: '2-digit',
-  //   minute: '2-digit',
-  // })
-  let currentTime = currentDate.toLocaleTimeString()
+  let currentTime = currentDate.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+  currentTime = currentTime.replace('AM', '').replace('PM', '')
+  // let currentTime = currentDate.toLocaleTimeString()
   clockElement.textContent = currentTime
 }
 
