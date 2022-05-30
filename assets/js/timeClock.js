a03c56b9028a5f1b8dec4743351a9e65fd71a9c6
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-05-30 15:30:27 +0800

    Add greeting js

--- assets/js/timeClock.js
+++ assets/js/timeClock.js
@@ -1,4 +1,5 @@
 const clockElement = document.querySelector('[data-clock]')
+const greetingElement = document.querySelector('[data-greeting]')
 
 function showTime() {
   let currentDate = new Date()
@@ -6,8 +7,23 @@ function showTime() {
     hour: '2-digit',
     minute: '2-digit',
   })
+  let currentHours = currentDate.getHours()
+  let currentMinutes = currentDate.getMinutes()
+  let currentMilitaryTime = `${currentHours}${currentMinutes}`
   currentTime = currentTime.replace('AM', '').replace('PM', '')
   clockElement.textContent = currentTime
+
+  setGreeting(currentMilitaryTime)
+}
+
+function setGreeting(time) {
+  if (time >= 0000 && time <= 1159) {
+    greetingElement.textContent = `Good morning, `
+  } else if (time >= 1200 && time <= 1759) {
+    greetingElement.textContent = `Good afternoon, `
+  } else if (time >= 1800 && time <= 2359) {
+    greetingElement.textContent = `Good evening, `
+  }
 }
 
 setInterval(showTime, 1000)
