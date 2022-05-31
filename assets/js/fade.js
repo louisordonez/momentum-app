ae2f8c3be774bbbbb58abe839237e143bd26e2f2
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-05-31 23:31:26 +0800

    Create js file depending on func

---
+++ assets/js/fade.js
@@ -0,0 +1,7 @@
+export const addBackgroundTransition = (element) => {
+  element.classList.add('fade-bg')
+}
+
+export const addAnimation = (element) => {
+  element.classList.add('fade-animation')
+}
