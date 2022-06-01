8b737ae452f0d9e5231977dae061552aeaf2a4fd
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-06-01 15:53:21 +0800

    Refactor currentMinutes var

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -11,7 +11,9 @@ const getTime = () => {
     minute: '2-digit',
   })
   let currentHours = date.getHours()
-  let currentMinutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes()
+  let currentMinutes = `${
+    date.getMinutes() < 10 ? '0' : ''
+  }${date.getMinutes()}`
   let currentMilitaryTime = `${currentHours}${currentMinutes}`
 
   return { currentRegularTime, currentMilitaryTime }
